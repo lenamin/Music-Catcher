892d2086d84a60668e8cf761b49da3d2d564daa7
lena <lenaminn@gmail.com>
2023-04-05 19:40:49 +0900

    [CHORE] project 세팅

---
+++ MusicCatcher/MusicCatcher/Extension/Color+Extension.swift
@@ -0,0 +1,44 @@
+//
+//  Color+Extension.swift
+//  MusicCatcher
+//
+//  Created by Lena on 2023/04/05.
+//
+
+
+import UIKit.UIColor
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+}
+
+extension UIColor {
+    /* 사용 예시
+    static var customSelectedGreen: UIColor {
+        return UIColor(rgb: 0x1C6974)
+    }
+     */
+    static var pointColor: UIColor {
+        return UIColor(rgb: 0xA822D7)
+    }
+    static var custombackgroundGrayColor: UIColor {
+        return UIColor(rgb: 0xFCFCFC)
+    }
+    static var customGradientPurple: UIColor {
+        return UIColor(rgb: 0xA312D7)
+    }
+}
---
+++ MusicCatcher/MusicCatcher/View/ReadyRecordViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AudioEditViewController.swift
+//  MusicCatcher
+//
+//  Created by Lena on 2023/04/05.
+//
+
+import UIKit
+
+class ReadyRecordViewController: UIViewController {
+    
+}
