8ec1ab284b2140e14a6f50d2ebbf366818495a1a
lena <lenaminn@gmail.com>
2023-04-05 19:53:21 +0900

    [CHORE] Foldering

---
+++ MusicCatcher/MusicCatcher/Global/Literal.swift
@@ -0,0 +1,8 @@
+//
+//  Literal.swift
+//  MusicCatcher
+//
+//  Created by Lena on 2023/04/05.
+//
+
+import Foundation
---
+++ MusicCatcher/MusicCatcher/View/HomeRecordCircleView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeRecordCircleView.swift
+//  MusicCatcher
+//
+//  Created by Lena on 2023/04/05.
+//
+
+import Foundation
