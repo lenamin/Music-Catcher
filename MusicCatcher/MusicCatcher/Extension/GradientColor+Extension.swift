21c8bb1621d0b8761ad3b9e002e663ceef3b6d33
lena <lenaminn@gmail.com>
2023-04-06 19:40:09 +0900

    [ADD] add setGradientColor method (#1)

--- MusicCatcher/MusicCatcher/Extension/GradientColor+Extension.swift
+++ MusicCatcher/MusicCatcher/Extension/GradientColor+Extension.swift
@@ -5,4 +5,21 @@
 //  Created by Lena on 2023/04/06.
 //
 
-import Foundation
+import UIKit.UIColor
+
+extension UIView {
+    
+    /// gradient 색상, gradient 방향을 결정
+    func setGradientColor(firstColor: UIColor,
+                          secondColor: UIColor,
+                          startPointNumber: CGPoint,
+                          endPointNumber: CGPoint) {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.colors = [firstColor.cgColor, secondColor.cgColor]
+        gradient.locations = [0.0, 1.0] // 이거 왜 필요하지?
+        gradient.startPoint = startPointNumber
+        gradient.endPoint = endPointNumber
+        gradient.frame = bounds
+        layer.addSublayer(gradient)
+    }
+}
