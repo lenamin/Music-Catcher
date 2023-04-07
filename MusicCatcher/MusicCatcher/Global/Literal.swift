6e15afeb0f0ac2e29ca111a0591f1640065990bb
lena <lenaminn@gmail.com>
2023-04-07 15:58:28 +0900

    [CHORE] add literal UIImages  (#1)

--- MusicCatcher/MusicCatcher/Global/Literal.swift
+++ MusicCatcher/MusicCatcher/Global/Literal.swift
@@ -15,6 +15,20 @@ let screenHeight = UIScreen.main.bounds.height
 
 let microphoneImageName: String = "microphone-white"
 
+let micImage: UIImage = UIImage(systemName: "mic") ?? UIImage()
+let micFillImage: UIImage = UIImage(systemName: "mic.fill") ?? UIImage()
+
+// for tabBarItem
+let fileImage: UIImage = UIImage(systemName: "folder") ?? UIImage()
+let fileFillImage: UIImage = UIImage(systemName: "folder.fill") ?? UIImage()
+
+let heartImage: UIImage = UIImage(systemName: "heart") ?? UIImage()
+let heartFillImage: UIImage = UIImage(systemName: "heart.fill") ?? UIImage()
+
+let gearImage: UIImage = UIImage(systemName: "gearshape") ?? UIImage()
+let gearFillImage: UIImage = UIImage(systemName: "gearshape.fill") ?? UIImage()
+
+
 // MARK: numbers
 
 let recordButtonWidth: Int = 216
