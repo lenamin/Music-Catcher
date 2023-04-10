bf89ee2a69817e62ae908af9dc1ce2099aa9e3f7
lena <lenaminn@gmail.com>
2023-04-10 14:57:38 +0900

    [CHORE] sceneDelegate 설정 (#1)

--- MusicCatcher/MusicCatcher/Global/SceneDelegate.swift
+++ MusicCatcher/MusicCatcher/Global/SceneDelegate.swift
@@ -10,14 +10,15 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    let rootViewController = TabBarController()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
-        window?.rootViewController = TabBarController()
+        window?.rootViewController = UINavigationController(rootViewController: rootViewController)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
