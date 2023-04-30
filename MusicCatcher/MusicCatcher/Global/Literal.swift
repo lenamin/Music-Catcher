3c3b69160a1c39048219193a9cbcce2385f5b62d
lena <lenaminn@gmail.com>
2023-04-30 11:16:50 +0900

    [CHORE] edit literals (#2)

--- MusicCatcher/MusicCatcher/Global/Literal.swift
+++ MusicCatcher/MusicCatcher/Global/Literal.swift
@@ -27,7 +27,7 @@ let analysisImage: UIImage = UIImage(systemName: "waveform.and.magnifyingglass")
 let gearImage: UIImage = UIImage(systemName: "gearshape") ?? UIImage()
 let gearFillImage: UIImage = UIImage(systemName: "gearshape.fill") ?? UIImage()
 
-let playImage: UIImage = UIImage(systemName: "play.fill") ?? UIImage()
+let recordImage: UIImage = UIImage(systemName: "circle.fill") ?? UIImage()
 let pauseImage: UIImage = UIImage(systemName: "pause.fill") ?? UIImage()
 let stopImage: UIImage = UIImage(systemName: "stop.fill") ?? UIImage()
 let listImage: UIImage = UIImage(systemName: "list.bullet") ?? UIImage()
